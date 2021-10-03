c2ca281afdebc581f51318a7e378b72eb10a8dac
sadra <sadrabelo@gmail.com>
2021-10-02 21:48:07 -0300

    metodo tabela criado

--- project/src/projeto/CalculadoraEsp.java
+++ project/src/projeto/CalculadoraEsp.java
@@ -11,16 +11,18 @@ public class CalculadoraEsp {
 * */
 
     public static void main(String[] args){
+        DadosAgrupados t = new DadosAgrupados();
         ArrayList<Double> moda;
         Collections.sort(Leitura.leitura());
 
-        double media, mediana, variancia, desvioPadrao, coeficienteVariacao;
+        double media, mediana, variancia, desvioPadrao, coeficienteVariacao, tabela;
         media = DadosBrutos.media(Leitura.leitura());
         mediana = DadosBrutos.mediana(Leitura.leitura());
         moda = DadosBrutos.moda(Leitura.leitura());
         variancia = DadosBrutos.variancia(Leitura.leitura());
         desvioPadrao = DadosBrutos.desvioPadrao(variancia);
         coeficienteVariacao = DadosBrutos.coeficienteVariacao(desvioPadrao, media);
+        t.tabela(Leitura.leitura(),DadosAgrupados.distriContinua(Leitura.leitura()));
 
         System.out.println("Media: " + media);
         System.out.println("Mediana: " + mediana);
@@ -28,6 +30,7 @@ public class CalculadoraEsp {
         System.out.println("Variancia: " + variancia);
         System.out.println("Desvio Padrão: " +desvioPadrao);
         System.out.println("Coeficiente de Variação: " +coeficienteVariacao);
+        System.out.println();
 
     }
 }
--- project/src/projeto/DadosAgrupados.java
+++ project/src/projeto/DadosAgrupados.java
@@ -3,14 +3,39 @@ package projeto;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class DadosAgrupados {
+public class    DadosAgrupados {
     public static ArrayList<Double> lista;
 
     public static Double distriContinua(ArrayList<Double> v){
-        double amplitudeTotal, raizAmostra;
+        double amplitudeTotal, raizAmostra, aRound;
         amplitudeTotal = v.get(v.size()-1) - v.get(0);
         raizAmostra = Math.sqrt(v.size()-1);
-        return amplitudeTotal/raizAmostra;
+        aRound = Math.round( (amplitudeTotal/raizAmostra) * 100.0 ) / 100.0;
+        return aRound;
+    }
+
+    public static void tabela (ArrayList<Double> v, Double distriContinua){
+        int cont = 0,frequencia = 0;
+        double limiteSuperior = v.get(0), limiteInferior = 0, limiteSuperiorRound, limiteInferiorRound;
+        System.out.println(distriContinua);
+        while(limiteSuperior < v.get(v.size() - 1)){
+            limiteInferior = limiteSuperior;
+            limiteSuperior += distriContinua;
+            frequencia = 0;
+            while(v.get(cont) < limiteSuperior && (cont + 1) < v.size() - 1) {
+                frequencia += 1;
+                cont += 1;
+                if((cont + 1) == v.size() - 1){
+                    cont += 1;
+                    frequencia += 2;
+                    break;
+                }
+            }
+            limiteSuperiorRound = Math.round( limiteSuperior * 100.0) / 100.0;
+            limiteInferiorRound = Math.round( limiteInferior * 100.0 ) / 100.0;
+            System.out.println(limiteInferiorRound + "|--" + limiteSuperiorRound + "|" + frequencia);
+        }
+
     }
 
 }
