8a61554c6c37b2e736dbfb1401e395bd9662a13b
BryanWille <84272231+BryanWille@users.noreply.github.com>
2021-11-01 19:11:14 -0300

    Adicionada a Logo

--- project/src/projeto/ParteGrafica.java
+++ project/src/projeto/ParteGrafica.java
@@ -16,14 +16,18 @@ public class ParteGrafica{
 
         public ParteGrafica(){
             janela = new JFrame("PresP");
+
             ImageIcon img = new ImageIcon(getClass().getResource("img/logo2.png"));
             janela.setIconImage(img.getImage());
+
             painelTabela = new JPanel();
             painelTabela.setLayout(new BorderLayout());
             this.janela.setSize(1280, 720);
             this.janela.setDefaultCloseOperation(janela.EXIT_ON_CLOSE);
+
             this.criarTabela();
             this.criarTitulo();
+
             this.janela.setVisible(true); //Deixar por último
         }
 
