951152793ec40f363392c6dd913011ffc33bbc44
BryanWille <84272231+BryanWille@users.noreply.github.com>
2021-11-05 09:01:32 -0300

    tentando fazer o layout

--- presp/src/projeto/InterfaceGrafica.java
+++ presp/src/projeto/InterfaceGrafica.java
@@ -10,14 +10,37 @@ import javax.swing.JLabel;
 import java.awt.Font;
 import javax.swing.SwingConstants;
 import java.awt.Color;
+import java.awt.Component;
+
 import javax.swing.ListSelectionModel;
 import javax.swing.JScrollPane;
 import javax.swing.JList;
 import javax.swing.JScrollBar;
 import javax.swing.JPanel;
 import java.awt.TextArea;
+import java.util.Objects;
 import java.awt.FlowLayout;
 import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JCheckBox;
+import java.awt.GridBagLayout;
+import javax.swing.JTextArea;
+import javax.swing.JSplitPane;
+import net.miginfocom.swing.MigLayout;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.RowSpec;
+import javax.swing.SpringLayout;
+import javax.swing.JDesktopPane;
+import java.awt.GridBagConstraints;
+import java.awt.CardLayout;
+import java.awt.Insets;
+import javax.swing.JTextField;
+import java.awt.Label;
+import com.jgoodies.forms.layout.FormSpecs;
+import java.awt.List;
+import javax.swing.JButton;
+import javax.swing.JTextPane;
 
 public class InterfaceGrafica {
 	private DadosAgrupados da = new DadosAgrupados();
@@ -25,9 +48,7 @@ public class InterfaceGrafica {
     private String[] colunas = {"Distribuicao de Frequencia", "Frequencia", "Frequencia Agrupada", "Ponto Medio"};
     
 	private JFrame frame;
-	private JScrollPane scrollPane;
 	private JTable table;
-	private JLabel lblNewLabel;
 
 	/**
 	 * Launch the application.
@@ -56,20 +77,111 @@ public class InterfaceGrafica {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 514, 332);
+		frame = new JFrame("PresP");
+		frame.setBounds(100, 100, 1010, 438);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(new BorderLayout(0, 0));
 		
-		scrollPane = new JScrollPane();
-		frame.getContentPane().add(scrollPane, BorderLayout.CENTER);
+		ImageIcon img = new ImageIcon(Objects.requireNonNull(getClass().getResource("logo.png")));
+        frame.setIconImage(img.getImage());
+		
+		JLabel lblNewLabel = new JLabel("Trabalho de Estat\u00EDstica");
+		lblNewLabel.setFont(new Font("Verdana", Font.BOLD, 18));
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		frame.getContentPane().add(lblNewLabel, BorderLayout.NORTH);
+		
+		JPanel panel = new JPanel();
+		frame.getContentPane().add(panel, BorderLayout.CENTER);
+		GridBagLayout gbl_panel = new GridBagLayout();
+		gbl_panel.columnWidths = new int[]{43, 0, -18, 38, 0, 46, 0, 0, 0, 0, 0, 0, 0, 0, 0, 54, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+		gbl_panel.rowHeights = new int[]{0, 0, 32, 0, 41, 42, 35, 35, 35, 39, 0, 0};
+		gbl_panel.columnWeights = new double[]{0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gbl_panel.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 0.0, Double.MIN_VALUE};
+		panel.setLayout(gbl_panel);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		GridBagConstraints gbc_scrollPane = new GridBagConstraints();
+		gbc_scrollPane.gridheight = 7;
+		gbc_scrollPane.gridwidth = 27;
+		gbc_scrollPane.insets = new Insets(0, 0, 5, 5);
+		gbc_scrollPane.fill = GridBagConstraints.BOTH;
+		gbc_scrollPane.gridx = 3;
+		gbc_scrollPane.gridy = 0;
+		panel.add(scrollPane, gbc_scrollPane);
 		
 		table = new JTable(dados, colunas);
 		scrollPane.setViewportView(table);
 		
-		lblNewLabel = new JLabel("Programa de Estatistica");
-		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel.setFont(new Font("Tw Cen MT Condensed", Font.BOLD, 20));
-		frame.getContentPane().add(lblNewLabel, BorderLayout.SOUTH);
+		JLabel lblNewLabel_1 = new JLabel("Decis");
+		lblNewLabel_1.setHorizontalAlignment(SwingConstants.LEFT);
+		GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
+		gbc_lblNewLabel_1.gridwidth = 2;
+		gbc_lblNewLabel_1.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_1.gridx = 0;
+		gbc_lblNewLabel_1.gridy = 7;
+		panel.add(lblNewLabel_1, gbc_lblNewLabel_1);
+		
+		JTextPane textPane = new JTextPane();
+		GridBagConstraints gbc_textPane = new GridBagConstraints();
+		gbc_textPane.gridwidth = 2;
+		gbc_textPane.insets = new Insets(0, 0, 5, 5);
+		gbc_textPane.fill = GridBagConstraints.BOTH;
+		gbc_textPane.gridx = 2;
+		gbc_textPane.gridy = 7;
+		panel.add(textPane, gbc_textPane);
+		
+		JButton btnNewButton = new JButton("New button");
+		GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
+		gbc_btnNewButton.insets = new Insets(0, 0, 5, 5);
+		gbc_btnNewButton.gridx = 4;
+		gbc_btnNewButton.gridy = 7;
+		panel.add(btnNewButton, gbc_btnNewButton);
+		
+		JLabel lblNewLabel_2 = new JLabel("Quartis");
+		lblNewLabel_2.setHorizontalAlignment(SwingConstants.LEFT);
+		GridBagConstraints gbc_lblNewLabel_2 = new GridBagConstraints();
+		gbc_lblNewLabel_2.gridwidth = 2;
+		gbc_lblNewLabel_2.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_2.gridx = 0;
+		gbc_lblNewLabel_2.gridy = 8;
+		panel.add(lblNewLabel_2, gbc_lblNewLabel_2);
+		
+		JTextPane textPane_1 = new JTextPane();
+		GridBagConstraints gbc_textPane_1 = new GridBagConstraints();
+		gbc_textPane_1.insets = new Insets(0, 0, 5, 5);
+		gbc_textPane_1.fill = GridBagConstraints.BOTH;
+		gbc_textPane_1.gridx = 3;
+		gbc_textPane_1.gridy = 8;
+		panel.add(textPane_1, gbc_textPane_1);
+		
+		JButton btnNewButton_1 = new JButton("New button");
+		GridBagConstraints gbc_btnNewButton_1 = new GridBagConstraints();
+		gbc_btnNewButton_1.insets = new Insets(0, 0, 5, 5);
+		gbc_btnNewButton_1.gridx = 4;
+		gbc_btnNewButton_1.gridy = 8;
+		panel.add(btnNewButton_1, gbc_btnNewButton_1);
+		
+		JLabel lblNewLabel_3 = new JLabel("Centis");
+		GridBagConstraints gbc_lblNewLabel_3 = new GridBagConstraints();
+		gbc_lblNewLabel_3.gridwidth = 2;
+		gbc_lblNewLabel_3.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_3.gridx = 0;
+		gbc_lblNewLabel_3.gridy = 9;
+		panel.add(lblNewLabel_3, gbc_lblNewLabel_3);
+		
+		JTextPane textPane_1_1 = new JTextPane();
+		GridBagConstraints gbc_textPane_1_1 = new GridBagConstraints();
+		gbc_textPane_1_1.insets = new Insets(0, 0, 5, 5);
+		gbc_textPane_1_1.fill = GridBagConstraints.BOTH;
+		gbc_textPane_1_1.gridx = 3;
+		gbc_textPane_1_1.gridy = 9;
+		panel.add(textPane_1_1, gbc_textPane_1_1);
+		
+		JButton btnNewButton_1_1 = new JButton("New button");
+		GridBagConstraints gbc_btnNewButton_1_1 = new GridBagConstraints();
+		gbc_btnNewButton_1_1.insets = new Insets(0, 0, 5, 5);
+		gbc_btnNewButton_1_1.gridx = 4;
+		gbc_btnNewButton_1_1.gridy = 9;
+		panel.add(btnNewButton_1_1, gbc_btnNewButton_1_1);
 	}
 }
--- presp/src/projeto/Leitura.java
+++ presp/src/projeto/Leitura.java
@@ -25,7 +25,7 @@ public class Leitura {
 
     public void criarLista(){
         ArrayList<Double> v = new ArrayList<>();
-        Scanner keyb = new Scanner(Leitura.class.getResourceAsStream("data/dadosbrutos8exercicio.txt"));
+        Scanner keyb = new Scanner(Leitura.class.getResourceAsStream("data/provateste.txt"));
         int i = 0;
         while (keyb.hasNextLine()) {
             v.add(i, Double.parseDouble(keyb.nextLine()));
